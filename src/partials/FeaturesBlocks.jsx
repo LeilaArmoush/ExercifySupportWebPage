3fc805150114ceea16f632deb31377a2904736c7
leila armoush <leila.armoush.1990@gmail.com>
2023-03-26 21:35:55 +0100

    minor corrections

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -9,10 +9,7 @@ function FeaturesBlocks() {
             {/* Section header */}
             <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
               <h2 className="h2 mb-4">Follow set cardio workouts or build custom workouts with spotify tunes to match your effort tempo</h2>
-
             {/* Items */}
-            <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
-
               {/* 1st item */}
               <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
                 <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
