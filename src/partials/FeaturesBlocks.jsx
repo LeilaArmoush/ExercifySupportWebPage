3aeeac70219c4f1bf4574e8cca2e14a67ade17ea
leila armoush <leila.armoush.1990@gmail.com>
2023-03-26 21:39:36 +0100

    update style

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -10,6 +10,8 @@ function FeaturesBlocks() {
             <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
               <h2 className="h2 mb-4">Follow set cardio workouts or build custom workouts with spotify tunes to match your effort tempo</h2>
             {/* Items */}
+            <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
+
               {/* 1st item */}
               <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
                 <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
@@ -85,9 +87,8 @@ function FeaturesBlocks() {
                 <h4 className="h4 mb-2">Spotify premium users only</h4>
                 <p className="text-lg text-gray-400 text-center">This app is only avaliable for spotify premium users. Spotify Premium lets you play any track, ad-free and with better audio quality. Go to spotify.com/premium to try it for free.</p>
               </div>
-
             </div>
-
+            </div>
           </div>
         </div>
       </section>
